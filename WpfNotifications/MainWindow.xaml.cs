7d4c1aa82e3e5f11fa0283c16c94289718994fe9
Denis Kobzar <layfilms.today@gmail.com>
2023-05-07 13:26:33 +0300

    Update Source

--- WpfNotifications/MainWindow.xaml.cs
+++ WpfNotifications/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,7 @@ namespace WpfNotifications
     /// </summary>
     public partial class MainWindow : Window
     {
-        readonly IList<Car> _cars = new List<Car>();
+        private readonly IList<Car> _cars = new ObservableCollection<Car>();
         public MainWindow()
         {
             InitializeComponent();
@@ -30,17 +31,36 @@ namespace WpfNotifications
                 Id = 1,
                 Color = "Blue",
                 Make = "Chevy",
-                PetName = "Kit"
+                PetName = "Kit",
+                IsChanged = false
             });
             _cars.Add(new Car()
             {
                 Id = 2,
                 Color = "Red",
                 Make = "Ford",
-                PetName = "Red Rider"
+                PetName = "Red Rider",
+                IsChanged = false
             });
 
             cboCars.ItemsSource = _cars;
+        }
+
+        private void btnChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            _cars.First(x => x.Id == ((Car)cboCars.SelectedItem)?.Id).Color = "Pink";
+        }
+
+        private void btnAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            var id = _cars.Last() == null ? 1 : _cars.Last().Id + 1;
+            _cars.Add(new Car
+            {
+                Id = id,
+                Color = "Yellow",
+                Make = "VM",
+                PetName = "Birdie"
+            });
         }
     }
 }
